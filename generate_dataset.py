f13e7edc20d8b78a0ee4a8714c6b0e7a4f8d55ff
Prerna Prasad <107390723+Prerna2022@users.noreply.github.com>
2023-04-10 19:25:15 +0530

    Add files via upload

---
+++ generate_dataset.py
@@ -0,0 +1,40 @@
+import cv2
+import os
+
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+face_id = input('enter your id ')
+cap = cv2.VideoCapture(0)
+
+#face Detection
+face_detector = cv2.CascadeClassifier("C:/Users/Prerna/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+count = 0
+
+assure_path_exists("F:\Dataset")
+while (True):
+    _, image_frame = cap.read()
+    gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        count += 1
+        # Save the captured image into the datasets folder
+        cv2.imwrite("F:\Dataset\\" + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+        cv2.imshow('frame', image_frame)
+
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+    elif count >= 50:
+        print("Successfully Captured")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
